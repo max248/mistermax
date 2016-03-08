fc9eb3fba85f41387ae016f02681e175457163f4
Genius <yoldoshev-1994@mail.ru>
2016-03-09 00:11:56 +0300

    Create code_error.go

---
+++ code_error.go
@@ -0,0 +1,24 @@
+func f1(a int, b int, a int) {
+	fmt.Print(a + b)
+}
+
+func f2(c int, b int) {
+	var a int
+	var a int
+	fmt.Print(a + b)
+}
+
+func f3() {
+	var a [][]int
+	a[1][2][3] = 4
+}
+
+func f4() {
+	var a bool
+	var b int
+	b = a + b
+}
+
+func main() {
+	fmt.Print("Hello, world!")
+}
